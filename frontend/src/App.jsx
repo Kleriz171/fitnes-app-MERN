21dd38a06a920b08bf2f55820c42906a15ac3b25
Kleriz171 <bibakleris@gmail.com>
2025-11-04 20:49:52 +0100

    Added a home page and a nav Bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,18 @@
-import './App.css'
+import { Routes, Route, BrowserRouter} from 'react-router-dom'
+//pages and components
+import Home from './Pages/home'
+import Navbar from './components/Navbar'
 
 function App() {
-
   return (
-    <>
-    </>
+    <BrowserRouter>
+    <Navbar/>
+    <div className='pages'>
+      <Routes>
+        <Route path='/' element = {<Home/>} />
+      </Routes>
+    </div>
+    </BrowserRouter>
   )
 }
 
