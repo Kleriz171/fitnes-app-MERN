72017b34508b1b9d69e8d9d3c56d2b4c7549a89a
Kleriz171 <bibakleris@gmail.com>
2025-11-05 14:02:41 +0100

    added a custon hook and added context. Website outputs new workout right away

--- frontend/src/Pages/home.jsx
+++ frontend/src/Pages/home.jsx
@@ -1,34 +1,37 @@
-import { useEffect, useState } from "react"
-import WorkoutDetails from "../components/workoutDetails"
-import WorkoutForm from "../components/workoutForm"
+import { useEffect } from "react";
+import { useWorkoutContext } from "../hooks/useWorkoutsContext";
 
-function Home(){
+// Components
+import WorkoutDetails from "../components/WorkoutDetails";
+import WorkoutForm from "../components/WorkoutForm";
 
-    const[workouts, setWorkouts] = useState(null)
+function Home() {
+  const { workouts, dispatch } = useWorkoutContext(); 
 
-    useEffect(()=>{
-        const fetchWorkouts = async()=>{
-            const response = await fetch('/api/workouts')
-            const json = await response.json()
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch("/api/workouts");
+      const json = await response.json();
 
-            if (response.ok){
-                setWorkouts(json)
-            }
-        }
+      if (response.ok) {
+        dispatch({ type: "SET_WORKOUTS", payload: json });
+      }
+    };
 
-        fetchWorkouts()
-    }, [])
+    fetchWorkouts();
+  }, [dispatch]);
 
-    return(
-        <div className="home">
-            <div className="workouts">
-                {workouts && workouts.map((workout)=> (
-                    <WorkoutDetails key= {workout._id} workout = {workout}/>
-                ))}
-            </div>
-            <WorkoutForm/>
-        </div>
-    )
+  return (
+    <div className="home">
+      <div className="workouts">
+        {workouts &&
+          workouts.map((workout) => (
+            <WorkoutDetails key={workout._id} workout={workout} />
+          ))}
+      </div>
+      <WorkoutForm />
+    </div>
+  );
 }
 
-export default Home
+export default Home;
--- frontend/src/components/workoutForm.jsx
+++ frontend/src/components/workoutForm.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react"
+import { useWorkoutContext } from "../hooks/useWorkoutsContext";
+
 
 function WorkoutForm (){
+    const {dispatch} = useWorkoutContext()
     const [title, setTitle] = useState("")
     const [load, setLoad] = useState("")
     const [reps, setReps] = useState("")
@@ -29,7 +32,8 @@ function WorkoutForm (){
             setLoad('')
             setReps('')
             setError(null)
-            console.log('new workout added')
+            console.log('new workout added', json)
+            dispatch({type: 'CREATE_WORKOUT', payload: json})
             
         }
     }
