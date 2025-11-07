eeb139ef519b85e828e32da422f8ff4c70bf94e1
Kleriz171 <bibakleris@gmail.com>
2025-11-07 20:10:52 +0100

    Add proper login

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -10,7 +10,15 @@ const createToken = (_id) =>{
 
 //login user
 const loginUser = async (req, res) =>{
-    res.json({msg: "login user"})
+    const {email, password} = req.body
+    try{
+        const user = await User.login(email, password)
+
+        const token = createToken(user._id)
+        res.status(200).json({email, token})
+    }catch (error){
+        res.status(400).json({error: error.message})
+    }
 }
 
 
@@ -25,8 +33,6 @@ const signupUser = async (req, res) =>{
     }catch (error){
         res.status(400).json({error: error.message})
     }
-
-    res.json({msg: "signup user"})
 }
 
 module.exports = {
