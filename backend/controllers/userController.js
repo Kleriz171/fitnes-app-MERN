b4c049f4da878418f9cabd932cf46bbb2ec7b1df
Kleriz171 <bibakleris@gmail.com>
2025-11-07 19:49:47 +0100

    Added token authentication

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,12 @@
 const User = require("../models/userModel")
-const mongoose = require("mongoose")
+const jwt = require('jsonwebtoken')
+require ('dotenv').config()
 
+console.log('SECRET:', process.env.SECRET)
+
+const createToken = (_id) =>{
+    return jwt.sign({_id}, process.env.SECRET, {expiresIn: '3d' })
+}
 
 //login user
 const loginUser = async (req, res) =>{
@@ -14,7 +20,8 @@ const signupUser = async (req, res) =>{
     try{
         const user = await User.signup(email, password)
 
-        res.status(200).json({email, user})
+        const token = createToken(user._id)
+        res.status(200).json({email, token})
     }catch (error){
         res.status(400).json({error: error.message})
     }
