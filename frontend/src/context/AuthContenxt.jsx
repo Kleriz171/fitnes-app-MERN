09c5553f9c8cfa60f4e02392b3342dd7279ec9ce
Kleriz171 <bibakleris@gmail.com>
2025-11-08 12:12:07 +0100

    Added authentication context

---
+++ frontend/src/context/AuthContenxt.jsx
@@ -0,0 +1,28 @@
+import { creaateContext, useReducer, useState } from "react";
+
+export const AuthContext =  createContext();
+
+export const authReducer = (state, action) =>{
+    switch(action.type) {
+        case 'LOGIN':
+            return {user: action.payload}
+        case 'LOGUT':
+            return {user: null}
+        default:
+            return state
+    }
+}
+
+export const AuthContextProvider = ({childern}) =>{
+    const [state,dispatch] = useReducer(authReducer,{
+        user: null
+    })
+
+    console.log('AuthContext state: ', state)
+
+    return(
+        <AuthContext.Provider>
+            {childern}
+        </AuthContext.Provider>
+    )
+}
