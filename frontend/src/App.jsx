202d245a07d3ca004923d1db86e17d79c367be5a
Kleriz171 <bibakleris@gmail.com>
2025-11-09 21:15:40 +0100

    finished

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,6 @@
-import { Routes, Route, BrowserRouter} from 'react-router-dom'
+import { Routes, Route, BrowserRouter, Navigate} from 'react-router-dom'
+import { useAuthContext } from './hooks/useAuthContext'
+
 //pages and components
 import Home from './Pages/home'
 import Navbar from './components/Navbar'
@@ -6,14 +8,23 @@ import Login from './Pages/login'
 import Signup from './Pages/signup'
 
 function App() {
+  const {user} = useAuthContext()
+
   return (
       <BrowserRouter>
       <Navbar/>
       <div className='pages'>
         <Routes>
-          <Route path='/' element = {<Home/>} />
-          <Route path='/login' element = {<Login/>}/>
-          <Route path='/signup' element = {<Signup/>}/>
+          <Route 
+          path='/' 
+          element = {user ? <Home/> : <Navigate to="/login"/>} />
+          <Route 
+          path='/login' 
+          element = {!user ? <Login/>: <Navigate to ="/"/>}  />
+          <Route 
+          path='/signup' 
+          element = {!user ? <Signup/>: <Navigate to ="/"/>}  
+          />
         </Routes>
       </div>
       </BrowserRouter>
--- frontend/src/hooks/useLogout.jsx
+++ frontend/src/hooks/useLogout.jsx
@@ -1,13 +1,16 @@
 import { useAuthContext} from './useAuthContext'
+import { useWorkoutContext } from './useWorkoutsContext'
 
 export const useLogout = () =>{
     const {dispatch} = useAuthContext()
+    const {dispatch: workoutsDispatch} = useWorkoutContext()
     const logout = ()  =>{
         localStorage.removeItem('user')
 
 
         //logout
         dispatch({type: 'LOGOUT'})
+        workoutsDispatch({type:'SET_WORKOUTS'}, null)
     }
 
     return{logout}
