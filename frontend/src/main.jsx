6e927116073c021c351779078e3ea672a35549f1
Kleriz171 <bibakleris@gmail.com>
2025-11-09 17:07:07 +0100

    Add conditional rendering for Navbar links based on user authentication status App can now remember logged-in users and display appropriate navigation options.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,11 +6,11 @@ import { WorkoutContextProvider } from "./context/WorkoutContext.jsx";
 import { AuthContextProvider } from "./context/AuthContext.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+
     <AuthContextProvider>
       <WorkoutContextProvider>
         <App />
       </WorkoutContextProvider>
     </AuthContextProvider>
-  </StrictMode>
+
 );
