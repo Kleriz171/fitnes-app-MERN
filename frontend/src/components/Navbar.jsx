be53521d82d732c0becd302616e49bd5b1e01d13
Kleriz171 <bibakleris@gmail.com>
2025-11-09 15:37:38 +0100

    Add a hook for logout

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,13 @@
 import { Link } from "react-router-dom"
+import { useLogout } from "../hooks/useLogout"
+
 
 function Navbar ()  {
+    const { logout } = useLogout()
+
+    const handleClick = () =>{
+        logout()
+    }
 
     return(
     <header>
@@ -9,6 +16,9 @@ function Navbar ()  {
             <h1>WorkoutBuddy</h1>
             </Link>
             <nav>
+                <div>
+                    <button  onClick={handleClick}>Log out</button>
+                </div>
             <div>
                 <Link to= '/login'>Login</Link>
                 <Link to= '/signup'>Signup</Link>
